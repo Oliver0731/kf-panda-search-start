a8285a9f2645c1034a60cae02653aa891106f58c
Oliver Townell <o.townell@EDU.epsb.ca>
2022-10-19 14:48:04 -0600

    m

--- main.js
+++ main.js
@@ -4,5 +4,22 @@
 document.getElementById("btn").addEventListener("click", btnClicked);
 
 function btnClicked() {
-  console.log("Search");
+  let name = document.getElementById("char-in").value;
+
+  //if statement - test th inout
+  if (name === "Po") {
+    console.log("Yes, Po is a KF Panda Character");
+    document.getElementById("char-name").innerHTML = name;
+  } else if (name === "Tigress") {
+    console.log(`yes, Tigress  is not a KF Panda character.`);
+    document.getElementById("char-name").innerHTML = name;
+  } else if (name === "Mantis") {
+    console.log("Yes, Mantis is a KF character.");
+    document.getElementById("char-name").innerHTML = name;
+  } else if (name === "Monkey") {
+    console.log("Yes, Monkey is a KF character.");
+    document.getElementById("char-name").innerHTML = name;
+  } else {
+    console.log(`No, ${name} is not a KFCU Character`);
+  }
 }
